46db25a8437e1850d372f23b7e8700fa350d9c76
ves-sev <newsecretword@yandex.ru>
2021-06-16 23:22:57 +0300

    html and styles added

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,35 @@
 $(function(){
 
+  var swiper = new Swiper('.portfolio__slider', {
+    slidesPerView: 1,
+    // centerInsufficientSlides:true,
+    // edgeSwipeDetection:true,
+    spaceBetween: 100,
+    navigation: {
+      nextEl: '.portfolio__slider-next',
+      prevEl: '.portfolio__slider-prev',
+    },
+    pagination: {
+      el: '.portfolio__slider-pagination',
+      clickable: true,
+    },
+  });
 
+  var swiper = new Swiper('.feedback__slider', {
+    slidesPerView: 1,
+    // effect: 'fade',
+    // spaceBetween: 200,
+    // cssMode:true,
+    observer:true,
+    navigation: {
+      nextEl: '.feedback__slider-next',
+      prevEl: '.feedback__slider-prev',
+    },
+    pagination: {
+      el: '.feedback__slider-pagination',
+      clickable: true,
+    },
+  });
 
 
 })
