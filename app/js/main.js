e3b5e56ac723b716d2032bdc8c59214878d40369
ves-sev <newsecretword@yandex.ru>
2021-06-23 17:48:15 +0300

    modal-window html added, styles changed, script added

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,35 @@
-$(function(){
+$(function () {
+
+  $('.header__nav-burger').on('click', function () {
+    $('.header__nav').toggle('slow', function () {
+          $('section, footer').css('filter', 'blur(3px)');
+        });
+  });
+
+  $('.header__nav-close').on('click', function () {
+    $('.header__nav').toggle('slow', function () {
+      $('section, footer').css('filter', '');
+    
+    });
+  });
+
+  $('.top__banner-btn, .cycle__item-btn, .examples__request-btn').on('click', function () {
+    $('.modal__request').toggle('slow', function () {
+      $('.content, .footer').css('filter', 'blur(3px)');
+      $('body').css('overflow', 'hidden');
+      
+    });
+  });
+  $('.modal__request-close').on('click', function () {
+    $('.modal__request').toggle('slow', function () {
+      $('.content, .footer').css('filter', '');
+      $('body').css('overflow', '');
+    });
+  });
 
   var swiper = new Swiper('.portfolio__slider', {
     slidesPerView: 1,
-    // centerInsufficientSlides:true,
-    // edgeSwipeDetection:true,
-    spaceBetween: 100,
+       spaceBetween: 100,
     navigation: {
       nextEl: '.portfolio__slider-next',
       prevEl: '.portfolio__slider-prev',
@@ -17,10 +42,8 @@ $(function(){
 
   var swiper = new Swiper('.feedback__slider', {
     slidesPerView: 1,
-    // effect: 'fade',
-    // spaceBetween: 200,
-    // cssMode:true,
-    observer:true,
+    effect: 'fade',
+    observer: true,
     navigation: {
       nextEl: '.feedback__slider-next',
       prevEl: '.feedback__slider-prev',
